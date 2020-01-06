9c4052bd93bbdb55cc6c606bd13c10cc9dc61681
Miguel Fernandez <miguelfernandez023@gmail.com>
2020-01-06 10:39:57 -0500

    added apiRoutes, htmlRoutes, server and friends.js

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Chandler Bing",
+        photo: "Image.png",
+        scores: [ , , , , , , ,  ,  ,]
+    },
+    {
+        name: "Rachel Green",
+        photo: "Image.png",
+        scores: [ , , , , , , ,  ,  ,]
+    },
+    {
+        name: "Ross Gellar",
+        photo: "Image.png",
+        scores: [ , , , , , , ,  ,  ,]
+    },
+    {
+        name: "Monica Gellar",
+        photo: "Image.png",
+        scores: [ , , , , , , ,  ,  ,]
+    },
+    {
+        name: "Phoebe Bouffay",
+        photo: "Image.png",
+        scores: [ , , , , , , ,  ,  ,]
+    },
+    {
+        name: "Joey Something",
+        photo: "Image.png",
+        scores: [ , , , , , , ,  ,  ,]
+    }
+];
+
+module.exports = friends;
---
+++ FriendFinder/server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ typpe: "application/vnd.api+json"}));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(port, () => console.log("Listening on port %s", port));
