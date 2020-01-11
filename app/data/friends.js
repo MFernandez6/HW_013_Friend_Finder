9a56f93b0794b6c9ae136ed988d47fadca82cc01
Miguel Fernandez <miguelfernandez023@gmail.com>
2020-01-11 13:54:16 -0500

    remove nested folder

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Chandler Bing",
+        photo: "https://lgbtqnation-assets.imgix.net/2019/10/matthew-perry-chandler-bing-gay.jpg?w=790&h=530&fit=crop&auto=format&auto=compress&crop=faces",
+        scores: [2, 1, 3, 3, 2, 5, 1, 1, 1, 1]
+    },
+    {
+        name: "Rachel Green",
+        photo: "https://i.pinimg.com/originals/45/4e/b9/454eb97d7bb947eab5203dbd975fb017.jpg",
+        scores: [2, 5, 2, 2, 3, 2, 2, 1, 1, 5]
+    },
+    {
+        name: "Ross Geller",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/9/9d/Ross_Unagi.jpg/revision/latest?cb=20190210055223",
+        scores: [5, 2, 1, 2, 2, 2, 1, 1, 3, 3]
+    },
+    {
+        name: "Monica Geller",
+        photo: "https://usercontent1.hubstatic.com/14597034.jpg",
+        scores: [2, 2, 5, 2, 3, 1, 2, 1, 5, 3]
+    },
+    {
+        name: "Phoebe Buffay",
+        photo: "https://www.lionsroar.com/wp-content/uploads/2019/11/phoebe-1.jpg",
+        scores: [2, 1, 2, 5, 3, 3, 1, 5, 2, 4]
+    },
+    {
+        name: "Joey Tribbiani",
+        photo: "https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE1XFxcLzEwXFxcLzI5MTExMzEyXFxcL0pvZXkuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZVxcXC9uby1pbWFnZS5wbmc_aWQ9MjY0YTJkYmUzNzBmMmM2NzVmY2RcIixcIm9wdGlvbnNcIjpbXX0iLCJoYXNoIjoiMDY2ODU4NGY3MDNjZjg1MWJiOWU1YTgxMDJlNWE4ZTUxMzBlNGI1MiJ9/joey.jpg",
+        scores: [2, 1, 1, 3, 5, 1, 5, 1, 1, 5]
+    }
+];
+
+module.exports = friends;
